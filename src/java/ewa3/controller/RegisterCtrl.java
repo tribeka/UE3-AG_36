0bbf1bc9299de79ebdf32f90c919aab11648d095
Bernhard Schreder <bernhard.schreder@gmx.at>
2012-05-05 19:12:33 +0200

    register.xhtml Validierung fertiggestellt. i18n Basisversion (für register) hinzugefügt

--- src/java/ewa3/controller/RegisterCtrl.java
+++ src/java/ewa3/controller/RegisterCtrl.java
@@ -4,6 +4,8 @@
  */
 package controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -50,7 +52,7 @@ public class RegisterCtrl {
     // Register new player
     public String register()
     {
-        return "/login.xhtml";
+        return "/index.xhtml";
     }
     
     //Checks if the display checkbox changed
@@ -66,12 +68,58 @@ public class RegisterCtrl {
     public void validateGeburtsdatum(FacesContext ctx, UIComponent component, Object value) throws ValidatorException
     {
         // @TODO 
-        String username = (String)value;
-
-        if(!username.equals("Markus") && !username.equals("Heidi"))
+        String dateofbirth = (String)value;
+        boolean validDate;
+        
+        String DATE_PATTERN = 
+          "(0?[1-9]|[12][0-9]|3[01])\\.(0?[1-9]|1[012])\\.((19|20)\\d\\d)";
+        Pattern pattern = Pattern.compile(DATE_PATTERN);
+        Matcher matcher = pattern.matcher(dateofbirth);
+        
+        if(matcher.matches()){
+ 
+	 matcher.reset();
+ 
+	 if(matcher.find()){
+ 
+             String day = matcher.group(1);
+	     String month = matcher.group(2);
+	     int year = Integer.parseInt(matcher.group(3));
+ 
+	     if (day.equals("31") && 
+		  (month.equals("4") || month .equals("6") || month.equals("9") ||
+                  month.equals("11") || month.equals("04") || month .equals("06") ||
+                  month.equals("09"))) {
+			validDate = false; // only 1,3,5,7,8,10,12 has 31 days
+	     } else if (month.equals("2") || month.equals("02")) {
+                  //leap year
+		  if(year % 4==0){
+			  if(day.equals("30") || day.equals("31")){
+				  validDate = false;
+			  }else{
+				  validDate = true;
+			  }
+		  }else{
+		         if(day.equals("29")||day.equals("30")||day.equals("31")){
+				  validDate = false;
+		         }else{
+				  validDate = true;
+			  }
+		  }
+	      }else{				 
+		validDate = true;				 
+	      }
+	   }else{
+    	      validDate = false;
+	   }		  
+     }else{
+	  validDate = false;
+     }			    
+        
+        if(!validDate)
         {
             FacesMessage msg = new FacesMessage(
-            FacesMessage.SEVERITY_WARN,"Wrong username!", null);
+            FacesMessage.SEVERITY_WARN,"Date format should be DD.MM.YYYY!", null);
             throw new ValidatorException(msg);
         }
     }
